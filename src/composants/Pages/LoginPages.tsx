5edc4749f4efc417406eb22565de4c43d662d4fa
Madeline <grammaticomadeline@hotmail.fr>
2023-12-01 16:37:40 +0100

    feat(F01) LOGIN > add Title and form

--- src/composants/Pages/LoginPages.tsx
+++ src/composants/Pages/LoginPages.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react"
+
 export function LoginPage() {
+    const [inputValue, setInputValue] = useState("")
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        alert(`Bonjour ${inputValue}`)
+        setInputValue('')
+    }
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value)
+    }
     return (
-        <div>LoginPage</div>
+        <div>
+            <h1>Bienvenue chez nous</h1>
+            <h2>Connectez vous</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Entrez votre prénom" onChange={handleChange} value={inputValue} required></input>
+                <button>Accédez à votre espace</button>
+            </form>
+        </div>
     )
 }
