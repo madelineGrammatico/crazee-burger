3cb3b84d1bbb813586c5a65202d2d6925c0d77a6
Madeline <grammaticomadeline@hotmail.fr>
2024-06-19 17:39:39 +0200

    (F03) : design system correction

--- src/theme/index.js
+++ src/theme/index.js
@@ -5,7 +5,6 @@ const colors = {
     background_white: "#f5f5f7",
     background_dark: "#292729",
     primary: "#ffa01b",
-    primary_burger: "#ff9f1b",
     green: "green",
     success: "#60bd4f",
     red: "#e25549",
