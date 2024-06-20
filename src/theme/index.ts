467156edacdecb6c233391fd214ffb7e9f9e56e4
Madeline <grammaticomadeline@hotmail.fr>
2024-06-20 10:38:20 +0200

    (F03) : refactoring design system

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -28,16 +28,26 @@ const colors = {
   }
   
   const fonts = {
-    XXXS: "8px",
-    XXS: "10px",
-    XS: "12px",
-    P0: "15px",
-    P1: "18px",
-    P2: "20px",
-    P3: "24px",
-    P4: "36px",
-    P5: "48px",
-    P6: "60px",
+    size: {
+      XXXS: "8px",
+      XXS: "10px",
+      XS: "12px",
+      P0: "15px",
+      P1: "18px",
+      P2: "20px",
+      P3: "24px",
+      P4: "36px",
+      P5: "48px",
+      P6: "60px"
+    },
+    weights:{
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+      heavy: 800
+    }
   }
   const gridUnit = 8
   const borderRadius = {
@@ -47,14 +57,6 @@ const colors = {
     circle: "50%",
   }
   const shadows = {}
-  const weights = {
-    light: 300,
-    regular: 400,
-    medium: 500,
-    semiBold: 600,
-    bold: 700,
-    heavy: 800,
-  }
   
   export const theme = {
     colors,
@@ -62,6 +64,5 @@ const colors = {
     gridUnit,
     borderRadius,
     shadows,
-    weights,
     spacing,
   }
