3fe76a1ba0106dcce42e5b888294872bf3dd4202
Madeline <grammaticomadeline@hotmail.fr>
2024-07-18 21:02:27 +0200

    (F04) : ORDERPAGE : create Profile

--- src/components/Pages/OrderPage/RightSideNav.tsx
+++ src/components/Pages/OrderPage/RightSideNav.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { useNavigate} from "react-router-dom"
 import styled from 'styled-components'
+import Profile from './Profile'
 
 export default function RightSideNav({userName}) {
-    const navigate = useNavigate()
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault()
-        navigate("/")
-    }
+    
     return (
     <RightSideNavStyled>
-        <h1>Bonjour { userName }</h1>  
-        <button onClick={ handleClick }>Déconnection</button>
-        </RightSideNavStyled>
+      {/* <div className='admin-button'>admin button</div> */}
+      <Profile userName= {userName}/>
+    </RightSideNavStyled>
+      
+        
   )
 }
 const RightSideNavStyled = styled.div`
-  background-color: purple;
   display: flex;
+  align-items: center;
+  padding-right: 50px;
+  .admin-button {
+  }
+  
 `;
