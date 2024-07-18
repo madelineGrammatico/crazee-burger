5bf9be17544d5b52df98c1fb4a9fd3bf8b109970
Madeline <grammaticomadeline@hotmail.fr>
2024-07-18 15:17:21 +0200

    (F04) : orderPage : Layout

--- src/components/Pages/OrderPage/OrderPage.tsx
+++ src/components/Pages/OrderPage/OrderPage.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { useNavigate, useParams } from "react-router-dom"
+import styled from "styled-components"
+import { theme } from "../../../theme"
 
 export default function OrderPage() {
     const { userName } = useParams()
@@ -9,9 +11,41 @@ export default function OrderPage() {
         navigate("/")
     }
   return (
-    <main>
-        <h1>Bonjour { userName }</h1>  
-        <button onClick={ handleClick }>Déconnection</button>
-    </main>  
+    <OrderPageStyled>
+      <div className="container">
+        <nav>
+          <h1>Bonjour { userName }</h1>  
+          <button onClick={ handleClick }>Déconnection</button>
+        </nav>
+        <main></main>
+      </div>
+      
+    </OrderPageStyled>  
   )
 }
+const OrderPageStyled = styled.div`
+  background-color: ${theme.colors.primary};
+  height:100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .container{
+    background-color: red;
+    display: flex;
+    height: 95vh;
+    flex-direction: column;
+    align-items: center;
+    width: 1400px;
+    nav{
+    background-color: blue;
+    }
+    main{
+      background-color: green;
+      width:100%;
+      flex: 1;
+      
+    }
+  }
+  
+`;
