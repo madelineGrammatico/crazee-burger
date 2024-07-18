19d670ad8755260df3134a10186fdfde174dd269
Madeline <grammaticomadeline@hotmail.fr>
2024-07-18 16:08:13 +0200

    (F04) : OrderPage : add sides to the navBar

--- src/components/Pages/OrderPage/NavBar.tsx
+++ src/components/Pages/OrderPage/NavBar.tsx
@@ -10,8 +10,12 @@ export default function NavBar({userName}: {userName: string}) {
     }
     return (
     <NavBarStyled>
+        <div className="left-Side">left</div>
+        <div className='Right-side'>
         <h1>Bonjour { userName }</h1>  
         <button onClick={ handleClick }>Déconnection</button>
+        </div>
+        
     </NavBarStyled>
   )
 }
@@ -21,4 +25,12 @@ const NavBarStyled = styled.nav`
 
     background-color: blue;
     display: flex;
+    justify-content: space-between;
+
+    .left-Side{
+        background-color: purple;
+    }
+    .Right-side{
+        background-color: purple;
+    }
 `;
