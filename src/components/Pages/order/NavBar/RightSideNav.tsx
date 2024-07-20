bb047472888725fa2a431d079bb7ac42ee0ff3e0
Madeline <grammaticomadeline@hotmail.fr>
2024-07-20 11:16:19 +0200

    (F06) : add state isAdmin

--- src/components/Pages/order/NavBar/RightSideNav.tsx
+++ src/components/Pages/order/NavBar/RightSideNav.tsx
@@ -1,11 +1,18 @@
 import styled from 'styled-components'
 import Profile from './Profile'
+import ToggleButton from '../../../reusables-ui/ToggleButton';
+import { useState } from 'react';
 
 export default function RightSideNav({userName} : {userName : string | undefined}) {
-    
+    const [isAdmin, setIsAdmin] = useState(false)
     return (
     <RightSideNavStyled>
-      {/* <div className='admin-button'>admin button</div> */}
+      <ToggleButton 
+        isChecked={isAdmin} 
+        onToggle={()=> setIsAdmin(!isAdmin)}
+        labelIfChecked = "DEACTIVER LE MODE ADMIN"
+        labelIfUnchecked = "ACTIVER LE MODE ADMIN"
+      />
       <Profile userName= {userName}/>
     </RightSideNavStyled>
       
@@ -16,6 +23,7 @@ const RightSideNavStyled = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px;
+  gap:50px;
   .admin-button {
   }
   
