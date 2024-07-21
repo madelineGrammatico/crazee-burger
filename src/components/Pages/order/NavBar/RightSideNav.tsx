3082c3fbaa72abca5eb4cf88be7b9536b156ea75
Madeline <grammaticomadeline@hotmail.fr>
2024-07-21 12:03:07 +0200

    (F06) : add toast without style

--- src/components/Pages/order/NavBar/RightSideNav.tsx
+++ src/components/Pages/order/NavBar/RightSideNav.tsx
@@ -2,18 +2,25 @@ import styled from 'styled-components'
 import Profile from './Profile'
 import ToggleButton from '../../../reusables-ui/ToggleButton';
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css"
 
 export default function RightSideNav({userName} : {userName : string | undefined}) {
     const [isAdmin, setIsAdmin] = useState(false)
+    const notify = () => {
+      setIsAdmin(!isAdmin)
+      toast("whoooo!")
+    }
     return (
     <RightSideNavStyled>
       <ToggleButton 
         isChecked={isAdmin} 
-        onToggle={()=> setIsAdmin(!isAdmin)}
+        onToggle={notify}
         labelIfChecked = "DEACTIVER LE MODE ADMIN"
         labelIfUnchecked = "ACTIVER LE MODE ADMIN"
       />
       <Profile userName= {userName}/>
+      <ToastContainer/>
     </RightSideNavStyled>
       
         
@@ -24,7 +31,5 @@ const RightSideNavStyled = styled.div`
   align-items: center;
   padding-right: 50px;
   gap:50px;
-  .admin-button {
-  }
   
 `;
