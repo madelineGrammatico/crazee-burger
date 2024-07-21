11a2656d033c2d18879e799ec84f57119f4dbfc5
Madeline <grammaticomadeline@hotmail.fr>
2024-07-21 20:38:40 +0200

    (F06) : toastAmin : add style

--- src/components/Pages/order/NavBar/RightSideNav.tsx
+++ src/components/Pages/order/NavBar/RightSideNav.tsx
@@ -4,12 +4,24 @@ import ToggleButton from '../../../reusables-ui/ToggleButton';
 import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
+import { FaUserSecret } from 'react-icons/fa';
+import { theme } from '../../../../theme';
 
 export default function RightSideNav({userName} : {userName : string | undefined}) {
     const [isAdmin, setIsAdmin] = useState(false)
-    const notify = () => {
+    const notify = async() => {
+      !isAdmin ? toast.info("Mode admin activé", {
+        icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      }): null
       setIsAdmin(!isAdmin)
-      toast("whoooo!")
     }
     return (
     <RightSideNavStyled>
@@ -32,4 +44,21 @@ const RightSideNavStyled = styled.div`
   padding-right: 50px;
   gap:50px;
   
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 `;
