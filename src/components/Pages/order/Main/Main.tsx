f68751c9103dedb3597f5366bc4d35445ce8f1ab
Madeline <grammaticomadeline@hotmail.fr>
2024-07-22 17:33:05 +0200

    (F07) : ADMIN : add layout in Main

--- src/components/Pages/order/Main/Main.tsx
+++ src/components/Pages/order/Main/Main.tsx
@@ -6,7 +6,10 @@ export default function Main() {
   return (
     <MainStyled>
       {/* <div className="basket">basket</div> */}
-      <Menu/>
+      <div className="menu-and-admin">
+        <Menu/>
+        <div className="admin">admin</div>
+      </div>
     </MainStyled>
   )
 }
@@ -14,15 +17,30 @@ const MainStyled = styled.div`
     width:100%;
 
     background-color: ${theme.colors.background_white};
-    flex: 1;
+    height: calc(95vh - 10vh);
     border-radius:  0 0 ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
     display: grid;
     grid-template-columns: 1fr;
     /* grid-template-columns: 25% 1fr; // for basket */
-
-    overflow-y: scroll;
+    overflow: hidden;
+    
     .basket{
       background-color: pink;
+    }
+    .menu-and-admin {
+      position: relative;
+      overflow-y: hidden;
+      display: grid;
+      
+      .admin{
+        position: absolute;
+        width: 100%;
+        height: 250px;
+        background-color : purple;
+        left:0;
+        bottom: 0%;
+        right:0;
+      }
     }
 `
