bb46d167d629997253bac5bc30ef610c74c0460f
Madeline <grammaticomadeline@hotmail.fr>
2024-07-29 20:43:21 +0200

    (F08) : ADMIN : fix error InputTextConfig

--- src/components/Pages/order/Main/Admin/panels/inputTextConfig.ts
+++ src/components/Pages/order/Main/Admin/panels/inputTextConfig.ts
@@ -5,21 +5,21 @@ import { NewProductType } from '../../../../../../lib/Types';
 
 export  const getInputTextsConfig =( newProduct : NewProductType)=>  [
     {
-      id:"0",
+      key:"0",
       name:"title",
       placeholder:"Nom du produit (ex: Super Burger)",
       value:newProduct.title,
       Icon:FaHamburger,
     },
     {
-      id: "1",
+      key: "1",
       name: "imageSource",
       placeholder: "Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)",
       value: newProduct.imageSource,
       Icon:  BsFillCameraFill,
     },
     {
-      id: "2",
+      key: "2",
       name: "price",
       placeholder: "Prix",
       value: newProduct.price ? newProduct.price.toString() : "",
