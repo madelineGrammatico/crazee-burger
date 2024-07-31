e2cf164563ce7687a6abbacc3ed7c117f95ca718
Madeline <grammaticomadeline@hotmail.fr>
2024-07-31 21:40:02 +0200

    (F08) :  REUSABLE_UI : refactoring button

--- src/components/reusables-ui/Button.tsx
+++ src/components/reusables-ui/Button.tsx
@@ -42,42 +42,54 @@ const getExtraSuccess  = () => css`
   }
 
 `
-const ExtraSizeLarge  = () =>  css`
-  height: 53px;
-  padding:  0 26px;
-  font-size: ${ theme.fonts.size.SM};
-`
+// const ExtraSizeLarge  = () =>  css`
+//   height: 53px;
+//   padding:  0 26px;
+//   font-size: ${ theme.fonts.size.SM};
+// `
 
-const ExtraSizeRegular  = () =>  css`
-  height: 34px;
-  padding: 0 26px;
-  font-size: ${ theme.fonts.size.XS};
-`
+// const ExtraSizeRegular  = () =>  css`
+//   height: 34px;
+//   padding: 0 26px;
+//   font-size: ${ theme.fonts.size.XS};
+// `
 
-const ExtraSizeSlim  = () => css`
-  height: 34px;
-  padding:  0 29px;
-  font-size: ${ theme.fonts.size.XS};
-`
-
+// const ExtraSizeSlim  = () => css`
+ 
+// `
+const HEIGHT = {
+  large:"53px",
+  regular:"34px",
+  slim:"34px"
+}
+const PADDING = {
+  large:"0 26px",
+  regular:"0 26px",
+  slim:"0 29px"
+}
+const FONT_SIZE = {
+  large: theme.fonts.size.SM,
+  regular: theme.fonts.size.XS,
+  slim: theme.fonts.size.XS
+}
 const EXTRA_STYLE = {
   primary: getExtraPrimary,
   success: getExtraSuccess,
 }
-const SIZESOPTIONS = {
+/* const SIZESOPTIONS = {
   large: ExtraSizeLarge,
   regular: ExtraSizeRegular,
   slim: ExtraSizeSlim
+} */
+
+const getEtraButtonStyle: (version: "primary" | "success") => () => RuleSet<object> = (version) => {
+  return EXTRA_STYLE[version]
 }
 
-const getButtonStyle: (version: "primary" | "success", size: "large" | "regular" | "slim") => (() => RuleSet<object>) | (() => RuleSet<object>) | (() => RuleSet<object>) = (version, size) => {
-  return EXTRA_STYLE[version] && SIZESOPTIONS[size]
-}
-
-
-
-const getBaseStyle =() => css`
-  
+const getBaseStyle =(size: "large" | "regular" | "slim") => css`
+   height: ${HEIGHT[size]};
+  padding: ${PADDING[size]};
+  font-size: ${FONT_SIZE[size]};
   width: fit-content;
   position: relative;
   display: inline-flex;
@@ -111,6 +123,7 @@ const getBaseStyle =() => css`
   }
 `
 const ButtonStyled = styled.button<ButtonStyledType>`
- ${({version, size}) => getButtonStyle(version, size)};
- ${ getBaseStyle()};
+${({size}) => getBaseStyle(size)};
+ ${({version}) => getEtraButtonStyle(version)};
+ 
 `
