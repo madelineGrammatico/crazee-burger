41a0687698f9e0eb8600c9fe3ad22e1e61fc4063
Madeline <grammaticomadeline@hotmail.fr>
2024-08-13 16:54:32 +0200

    (F11) :  UTILS : create findProductIn()

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,1 +1,5 @@
+import { ProductsType } from "../lib/types"
+
 export const deepClone = (array: object) => JSON.parse(JSON.stringify(array))
+
+export const findProductIn = (array: ProductsType, id: string) => array.find((item) => item.id === id)
