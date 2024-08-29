a1f187ee9ec73ddb0ff245ee368000848b6c4747
Madeline <grammaticomadeline@hotmail.fr>
2024-08-29 13:50:32 +0200

    F13 : API : LoginForm :  refactoring

--- src/components/Pages/login/LoginForm.tsx
+++ src/components/Pages/login/LoginForm.tsx
@@ -8,6 +8,7 @@ import { theme } from '../../../theme'
 import { BsPersonCircle } from 'react-icons/bs'
 import { IoChevronForward } from 'react-icons/io5'
 import { authenticateUser} from '../../../api/user'
+import Welcome from './Welcome'
 
 
 export default function LoginForm() {
@@ -26,9 +27,7 @@ export default function LoginForm() {
     
     return (
     <LoginFormStyled onSubmit={handleSubmit}>
-        <h1>Bienvenue chez nous !</h1>
-        <hr/>
-        <h2>Connectez vous</h2>
+        <Welcome/>
         <TextInput
           value={username} 
           onChange={handleChange} 
@@ -56,21 +55,6 @@ const LoginFormStyled = styled.form`
   border-radius: ${ theme.borderRadius.round };
   font-family: "Amatic SC", cursive;
 
-  hr{
-    border: 1.5px solid ${ theme.colors.loginLine };
-    margin-bottom: ${ theme.gridUnit * 5 }px;
-  }
-
-  h1{
-    color: ${ theme.colors.white };
-    font-size: ${ theme.fonts.size.P5};
-  }
-
-  h2{
-    color: ${ theme.colors.white };
-    margin: 20px 10px 10px;
-    font-size: ${ theme.fonts.size.P4};
-  }
   button{
     width:100%;
   }
